285673e3bdc9b1ded3f90ec0ae98eaee7ea57336
jrsmth <jamesrichardsmith97@gmail.com>
2025-07-09 18:48:02 +0100

    [patch] BLE-162: save audio in s3

--- service/src/main/kotlin/game/bible/passage/audio/AudioService.kt
+++ service/src/main/kotlin/game/bible/passage/audio/AudioService.kt
@@ -3,6 +3,7 @@ package game.bible.passage.audio
 import game.bible.passage.generation.GenerationService
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Service
+import game.bible.common.util.resource.S3BucketService
 
 private val log = KotlinLogging.logger {}
 
@@ -12,25 +13,21 @@ private val log = KotlinLogging.logger {}
  */
 @Service
 class AudioService(
-//    private val bucket: S3BucketService,
+    private val bucket: S3BucketService,
     private val generator: GenerationService) {
 
-    /** Generates the audio for a given passage and retrieves it from storage */
-    fun retrieveAudio(passageKey: String): ByteArray { // fixme
-        return generateAudio(passageKey)
-//        val entry = bucket.findAudio(passageKey)
-//
-//        return if (entry.isPresent) {
-//            entry.get()
-//
-//        } else generateAudio(passageKey)
+    /** Retrieves the audio for a given passage */
+    fun retrieveAudio(passageKey: String): ByteArray {
+
+        return bucket.getAudio(passageKey) ?: generateAudio(passageKey)
     }
 
+    /** Handles audio generation and retrieval from storage */
     private fun generateAudio(passageKey: String): ByteArray {
         log.info { "No entry exists for [$passageKey]! Generating audio" }
         val passageAudio = generator.audio(passageKey)
 
-//        return bucket.saveAudio(passageAudio)
+        bucket.uploadAudio(passageKey, passageAudio)
         return passageAudio
     }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
         }
 
         create("libs") {
-            from("game.bible:version:0.3.5")
+            from("game.bible:version:0.3.10")
         }
     }
 }
