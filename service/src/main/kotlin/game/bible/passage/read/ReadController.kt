10060764516068e3fa1d4ae4195aa09f1898af3e
Oj Tayo <o.tayo@hotmail.co.uk>
2025-10-11 10:46:01 +0100

    BLE-253: Included RequestParam Reference

--- service/src/main/kotlin/game/bible/passage/read/ReadController.kt
+++ service/src/main/kotlin/game/bible/passage/read/ReadController.kt
@@ -4,6 +4,7 @@ import game.bible.config.model.integration.BibleApiConfig
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
