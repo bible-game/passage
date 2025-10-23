1e0d592e1e3fd5a6aa9d07dd0aa7ca0bd82d49bc
jrsmth <jamesrichardsmith97@gmail.com>
2025-10-23 17:35:13 +0100

    [test] BLE-263: gradle settings update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,19 +3,19 @@ rootProject.name = "passage"
 include("service", "model")
 
 dependencyResolutionManagement {
-    versionCatalogs {
-        repositories {
-            mavenLocal()
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/bible-game/version")
-                credentials {
-                    username = System.getenv("GH_PACKAGES_USER") ?: System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GH_PACKAGES_TOKEN") ?: System.getenv("GITHUB_TOKEN")
-                }
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/bible-game/version")
+            credentials {
+                username = System.getenv("GH_PACKAGES_USER") ?: System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GH_PACKAGES_TOKEN") ?: System.getenv("GITHUB_TOKEN")
             }
         }
+    }
 
+    versionCatalogs {
         create("libs") {
             from("game.bible:version:0.6.1")
         }
