aa3d4e63716db881467da22d9e2962ad15d20466
jrsmth <jamesrichardsmith97@gmail.com>
2025-10-23 17:50:38 +0100

    [test] BLE-263: revert recent changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,19 +3,19 @@ rootProject.name = "passage"
 include("service", "model")
 
 dependencyResolutionManagement {
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        maven {
-            url = uri("https://maven.pkg.github.com/bible-game/version")
-            credentials {
-                username = System.getenv("GH_PACKAGES_USER") ?: System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GH_PACKAGES_TOKEN") ?: System.getenv("GITHUB_TOKEN")
+    versionCatalogs {
+        repositories {
+            mavenLocal()
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/bible-game/version")
+                credentials {
+                    username = System.getenv("GH_PACKAGES_USER")
+                    password = System.getenv("GH_PACKAGES_TOKEN")
+                }
             }
         }
-    }
 
-    versionCatalogs {
         create("libs") {
             from("game.bible:version:0.6.1")
         }
